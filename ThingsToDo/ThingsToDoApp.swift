46e8e16de4e3b4267cb51dc1cec87e5d96f6216b
varadasainikhil <varadasainikhil@gmail.com>
2025-03-21 16:24:41 -0400

    Initial Commit

---
+++ ThingsToDo/ThingsToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThingsToDoApp.swift
+//  ThingsToDo
+//
+//  Created by Sai Nikhil Varada on 3/21/25.
+//
+
+import SwiftUI
+
+@main
+struct ThingsToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

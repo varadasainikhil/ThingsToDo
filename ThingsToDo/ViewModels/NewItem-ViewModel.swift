f715279b4e50283b1e9a824f1c0b67cee95a05ec
varadasainikhil <varadasainikhil@gmail.com>
2025-03-27 14:07:17 -0400

    Completed the save functionality for the adding a new task.

--- ThingsToDo/ViewModels/NewItem-ViewModel.swift
+++ ThingsToDo/ViewModels/NewItem-ViewModel.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
 
 class NewItemViewViewModel : ObservableObject{
     @Published var title : String = ""
@@ -16,6 +18,27 @@ class NewItemViewViewModel : ObservableObject{
     }
     
     func save(){
+        
+        // get the current User id
+        guard let uID = Auth.auth().currentUser?.uid else{
+            return
+        }
+        
+        // create a model
+        let newId = UUID().uuidString
+        let newItem = ToDoListItem(id: newId, title: title, dueDate: dueDate.timeIntervalSince1970, createdDate:Date().timeIntervalSince1970, isDone: false)
+        
+        // save the model
+        let db = Firestore.firestore()
+        
+        db.collection("users")
+            .document(uID)
+            .collection("todos")
+            .document(newId)
+            .setData(newItem.asDictionary())
+        
+        
+        
         
     }
 }
